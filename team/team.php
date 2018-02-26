d8ab899130b9a34c4408e72ac8d1cce9cda7176b
priyansh <priyansh.shah1997@gmail.com>
2018-02-26 22:35:12 +0530

    First commit

---
+++ team/team.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/custom11.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Team</title>
+  </head>
+  <body>
+   <div id="app" class="container">
+        <div class="row"><div class="col-sm-4"></div>
+        <div class="col-sm-4"><h1>Main Coordinator</h1></div>
+        <div class="col-sm-4"></div></div>
+        <!-- 1st row-->
+        <div class="row">
+          <div class="col-sm-4">
+    <div class="hovereffect">
+        <img class="img-responsive" src="https://drive.google.com/open?id=1UHHiyJGnOPZFhr_D8GYhNE2ntu0cZ0LV" alt="" style="max-height: 200px;width: 100%;">
+        <div class="overlay">
+           <h2>Rahul Ghevariya</h2>
+           <a class="info">9913872121</a>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+    <div class="hovereffect">
+        <img class="img-responsive" src="https://drive.google.com/open?id=1viGLU8GtR_vjFIHxywmdsaVz8XjJO4O9" alt="" style="max-height: 200px;width: 100%;">
+        <div class="overlay">
+           <h2>Binoy Oza</h2>
+           <a class="info">7600411922</a>
+        </div>
+    </div>
+</div>
+<div class="col-sm-4">
+    <div class="hovereffect">
+        <img class="img-responsive" src="https://drive.google.com/open?id=1y3J3isyJk5OW3CuY09LhyuqFo3hMImkp" alt="" style="max-height: 200px;width: 100%;">
+        <div class="overlay">
+           <h2>Raj Adeshra</h2>
+           <a class="info">7567484809</a>
+        </div>
+    </div>
+</div><br>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!---->
+  </body>
+</html>
